ec832e48b9c14f4026df17a0c3f565535a922f54
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2023-01-18 13:55:16 -0600

    first commit, UI set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { FiMenu } from 'react-icons/fi';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [menu, setMenu] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="menu-icon-container" onClick={() => menu ? setMenu(false) : setMenu(true)}>
+          <FiMenu className="menu-icon"/>
+        </div>
+        <div className="app-title-container">
+          <h2 className="app-title">Breathing App</h2>
+        </div>
+      </div>
+      <div className="menu" style={menu ? {display: "block"} : {display: "none"}}>
+        <div className="menu-item-container">
+          <div className="menu-item">
+            Techniques
+          </div>
+        </div>
+        <div className="menu-item-container">
+          <div className="menu-item">
+            Resources
+          </div>
+        </div>
+        <div className="menu-item-container">
+          <div className="menu-item">
+            Presets
+          </div>
+        </div>
+        <div className="menu-item-container">
+          <div className="menu-item">
+            Learn
+          </div>
+        </div>
+        <div className="menu-footer">
+          <div className="menu-footer-items">
+            <div className="menu-item-container">
+              <div className="menu-item">
+                About us
+              </div>
+            </div>
+            <div className="menu-item-container">
+              <div className="menu-item">
+                Settings
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
